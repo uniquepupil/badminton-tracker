e644d1c758588583e6498625be704457138b8d1b
Parikshit <94772029+uniquepupil@users.noreply.github.com>
2026-08-09 19:52:31 +0530

    Update eslint.config.mjs

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-
+ 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
